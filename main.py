0eb17e9530de0f2f29cfaa7bc658d5b2246d8bc4
Abhisheak Sharma <zerodeaths693@gmail.com>
2023-04-27 06:56:57 +0530

    add updated model

--- main.py
+++ main.py
@@ -24,13 +24,21 @@ class Item(BaseModel):
     data: List[int]
 
 
-with open('linear_regression.pkl', 'rb') as f:
+with open('lstm_model.pkl', 'rb') as f:
     model = pickle.load(f)
+    
+with open('scaler.pkl', 'rb') as s:
+    sc = pickle.load(s)
+
 
 @app.post('/ml/predict')
 async def predict_endpoint(item: Item):
     data = item.data
-    yhat = model.predict(np.array([data]))
-    ans = yhat[0]
-    #print(yhat)
+    X_new = np.array([data])
+    X_new = sc.transform(X_new)
+    y_new = model.predict(X_new[:, 1:])
+    X_new[0][10] = y_new
+    temp_sc = sc.inverse_transform(X_new)
+    temp_sc = np.round(temp_sc)
+    ans =  temp_sc[0][-1]
     return {"Prediction": ans}
